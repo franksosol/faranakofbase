d049cf9c3005a33d1fc259e59ed54329737cc909
frank sosol <134972548+franksosol@users.noreply.github.com>
2023-07-29 17:02:43 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x6FEf6C2AA494D4ba61E499bd6de37A7800abccef";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
